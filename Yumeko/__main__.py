29381c0527f9ac4611988f63b56b669463443095
slayer <meerutuniversity996@gmail.com>
2025-07-29 10:37:22 +0530

    Update __main__.py

--- Yumeko/__main__.py
+++ Yumeko/__main__.py
@@ -175,11 +175,13 @@ async def start_cmd(_, message : Message):
     await message.react("🍓" , big = True)
     
     x = await message.reply_text(f"`Hie {message.from_user.first_name} <3`")
-    await sleep(0.3)
-    await x.edit_text("🐾")
-    await sleep(0.6)
-    await x.edit_text("❄️")
-    await sleep(0.6)
+     await sleep(0.3)
+    await x.edit_text("🧋")
+    await sleep(0.8)
+    await x.edit_text("🎊")
+    await sleep(0.8)
+    await x.edit_text("🎊")
+    await sleep(0.8)
     await x.delete()
     
     await message.reply_cached_media(file_id = STICKER_FILE_ID)    
